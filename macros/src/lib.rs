49e6ea0d5ed1e38b2f6ceaca7600fb24e2355b89
jabu <jabu@gx.ag>
2023-02-06 13:03:31 -0600

    add iyes scenes support

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -24,3 +24,27 @@ pub fn derive_scene(input: TokenStream) -> TokenStream {
         }
     })
 }
+
+#[proc_macro_derive(IyesScenes)]
+pub fn derive_iyes_scene(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = &input.ident;
+    let mut quotes = vec![];
+    if let Data::Enum(enm) = input.data {
+        for variant in enm.variants.iter() {
+            let variant_ident = &variant.ident;
+            quotes.push(quote! {
+                app.add_exit_system(#ident::#variant_ident, bodge::bevy::cleanup_non_persistent_entities);
+            });
+        }
+    }
+    TokenStream::from(quote! {
+        impl IyesScenes for #ident {
+            fn init_scene(app: &mut bevy::app::App) {
+                use iyes_loopless::prelude::AppLooplessStateExt;
+                app.add_loopless_state(#ident::default());
+                #(#quotes)*
+            }
+        }
+    })
+}
--- src/bevy/scenes.rs
+++ src/bevy/scenes.rs
@@ -1,16 +1,25 @@
 use bevy::{ecs::schedule::StateData, prelude::*};
-pub use bodge_macros::Scenes;
+pub use bodge_macros::{IyesScenes, Scenes};
 
 pub trait Scenes {
     fn init_scene(app: &mut App);
 }
 
+pub trait IyesScenes {
+    fn init_scene(app: &mut App);
+}
+
 pub trait AddScenes {
     fn add_scenes<T: Scenes + Default + StateData>(&mut self) -> &mut Self;
+    fn add_iyes_scenes<T: IyesScenes + Default + StateData>(&mut self) -> &mut Self;
 }
 
 impl AddScenes for App {
     fn add_scenes<T: Scenes + Default + StateData>(&mut self) -> &mut Self {
+        T::init_scene(self);
+        self
+    }
+    fn add_iyes_scenes<T: IyesScenes + Default + StateData>(&mut self) -> &mut Self {
         T::init_scene(self);
         self
     }
